fa952a2a9f4801d86b839543aca616b17f388ff2
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-12-24 00:28:41 +0300

    WIP - 1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#181818",
+        "gradient-blue": "#3C33EF",
+        "gradient-red": "#F43366",
+        "gray-custom": "#666666",
+      },
+      fontFamily: {
+        primary: ['"Poppins"'],
+        secondary: ['"Plus Jakarta Sans"'],
+        button: ['"Urbanist"'],
+        inter: ['"Inter"'],
+      },
+    },
+  },
+};
