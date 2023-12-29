82db893ee3f354e9fdb223c8d0fc4626253407c9
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-12-29 11:01:48 +0300

    desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./main.js", "./src/**/*.{js}"],
+  content: ["./index.html", "./main.js", "./src/**/*.js"],
   theme: {
     extend: {
       colors: {
